2085cdfa4fc8285db084a3fa7c8b4eb349b2e7a0
Jeremy <jeremy.savor@gmail.com>
2022-12-07 23:39:03 +0100

    Add solution to part 1 of day 7

--- aoc_2022_python/day7.py
+++ aoc_2022_python/day7.py
@@ -1,59 +1,77 @@
 from aoc import input_fpath_for_day
+import json
 
 
-class FileType:
-    def __init__(self, is_file: bool):
-        self.__is_file = is_file
-
-    def is_file(self):
-        return self.__is_file
-
-    def is_dir(self):
-        return not self.is_file()
+def parse_input(puzzle_input):
+    for line in puzzle_input.splitlines():
+        yield line.split(" ")
 
 
-class File(FileType):
-    def __init__(self, name: str, size: str):
-        super().__init__(is_file=True)
-        self.name = name
-        self.size = size
+def calc_directory_sizes(fs: dict, cur_name: str):
+    sizes = dict()
+    cur_size = 0
+    for (name, ftype) in fs.items():
+        if isinstance(ftype, dict) and name != "..":
+            sizes[name] = calc_directory_sizes(ftype, name)
+            cur_size += sizes[name][name]
+        else:
+            if ftype and name != ".." and name != cur_name:
+                cur_size += ftype[-1]  # (name, file size)
+
+    sizes[cur_name] = cur_size
+    return sizes
 
 
-class Dir(FileType):
-    def __init__(self, name: str):
-        super().__init__(is_file=False)
-        self.name = name
-        self.files = dict()
-
-    def dir(self, dirname: str):
-        return self.files[dirname]
-
-    def mkdir(self, dirname: str):
-        self.files[dirname] = dict()
-
-    def touch(self, fname: str, size: int):
-        self.files[fname] = File(fname, size)
+def print_fs(fs: dict, space=""):
+    for (k, v) in fs.items():
+        if isinstance(v, dict) and k != "..":
+            print(space, "-", k, "(dir)")
+            print_fs(v, space + " ")
+        else:
+            if k != "..":
+                print(space, "-", k, f"(file, size={v[1]})")
 
 
-class FileSystem:
-    root = "/"
+def filter_sizes_under_value(sizes, value):
+    filtered = list()
+    for (k, v) in sizes.items():
+        if isinstance(v, dict):
+            filtered += filter_sizes_under_value(v, value)
+        else:
+            if v <= value:
+                filtered.append(v)
 
-    def __init__(self):
-        self.root = Dir(self.root)
-        self.cwd = self.root
-
-    def cd(self, dirname):
-        self.cwd = self.cwd.dir(dirname)
-
-    def mkdir(self, dirname):
-        self.cwd.mkdir(dirname)
-
-    def touch(self, fname: str, size: int):
-        self.cwd.touch(fname, size)
+    return filtered
 
 
 def part_1(puzzle_input):
-    pass
+    insts = parse_input(puzzle_input)
+    parent = None
+    fs = {"/": {"..": parent}}
+    root = fs["/"]
+    cwd = root
+    for fst, *rest in insts:
+        if fst == "$":
+            cmd = rest[0]
+            if cmd == "cd":
+                dirname = rest[-1]
+                if dirname == "/":
+                    continue
+                parent = cwd
+                cwd = cwd[dirname]
+            elif fst == "ls":
+                continue
+        elif fst == "dir":
+            dirname = rest[-1]
+            cwd[dirname] = {"..": cwd}
+        else:  # New File
+            size = int(fst)
+            fname = rest[-1]
+            cwd[fname] = (fname, size)
+
+    #  print_fs(fs)
+    sizes = calc_directory_sizes(fs["/"], "/")
+    return sum(filter_sizes_under_value(sizes, 100000))
 
 
 def part_2(puzzle_input):
@@ -62,7 +80,6 @@ def part_2(puzzle_input):
 
 if __name__ == "__main__":
     with input_fpath_for_day(7).open() as f:
-        content = f.read()
         content = """\
 $ cd /
 $ ls
@@ -87,6 +104,7 @@ $ ls
 8033020 d.log
 5626152 d.ext
 7214296 k"""
+        content = f.read()
         print("Day 7")
         print("Part 1:", part_1(content))
         print("Part 2:", part_2(content))
