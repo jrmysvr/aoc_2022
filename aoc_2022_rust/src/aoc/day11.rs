09ab4671bd72b0fcb4681c434a625e84d9b63e2c
Jeremy <jeremy.savor@gmail.com>
2022-12-13 15:44:45 +0100

    Continue day 11

--- aoc_2022_rust/src/aoc/day11.rs
+++ aoc_2022_rust/src/aoc/day11.rs
@@ -21,12 +21,74 @@ struct Monkey {
     test: fn(WorryLevel) -> MonkeyId,
 }
 
-fn monkey_from(lines: Vec<&str>) -> Monkey {
+fn monkey_id_from(line: &str) -> MonkeyId {
+    line.trim()
+        .trim_start_matches("Monkey ")
+        .trim_end_matches(":")
+        .parse::<MonkeyId>()
+        .unwrap()
+}
 
+fn starting_items_from(line: &str) -> Vec<WorryLevel> {
+    line.trim()
+        .trim_start_matches("Starting items: ")
+        .split(", ")
+        .map(|item| item.parse::<WorryLevel>().unwrap())
+        .collect::<Vec<WorryLevel>>()
+}
+
+/*
+enum Op {
+    Add,
+    Mul,
+}
+*/
+
+type Op = fn() -> WorryLevel;
+
+struct Operation {
+    op: Op,
+    lval: WorryLevel,
+    rval: WorryLevel,
+}
+
+impl Operation {
+    /*
+    fn new(op_char: char, lval: WorryLevel, rval: WorryLevel) -> Self {
+        let op = match op_char {
+            // '+' => Op::Add,
+            // '*' => Op::Mul,
+            '+' => || -> WorryLevel { lval + rval },
+            '*' => || -> WorryLevel { lval * rval },
+            _ => panic!("Unsupported operation char: {op_char}"),
+        };
+
+        Self {
+            op: op,
+            lval: lval,
+            rval: rval,
+        }
+    }
+
+    fn apply(&self, worry_level: WorryLevel) -> WorryLevel {
+        0
+    }
+
+    fn f(&self, worry_level: WorryLevel) -> fn(WorryLevel) -> WorryLevel {
+        self.op
+    }
+    */
+}
+
+fn operation_from(line: &str) -> fn(WorryLevel) -> WorryLevel {
+    |worry| -> WorryLevel { 0 }
+}
+
+fn monkey_from(lines: Vec<&str>) -> Monkey {
     Monkey {
-        id: 0,
-        starting_items: Vec::<WorryLevel>::new(),
-        operation: |worry| -> WorryLevel { 0 },
+        id: monkey_id_from(lines[0]),
+        starting_items: starting_items_from(lines[1]),
+        operation: operation_from(lines[2]),
         test: |worry| -> MonkeyId { 1 },
     }
 }
@@ -75,6 +137,33 @@ Monkey 3:
 
     fn test_content() -> String {
         String::from(TEST_CONTENT)
+    }
+
+    #[test]
+    fn test_parse_operation() {
+        let example_operation = "Operation: new = old * 19";
+        let cleaned_operation = example_operation
+            .trim()
+            .trim_start_matches("Operation: new =")
+            .replace(" ", "")
+            .as_str();
+
+        let op_str = "+";
+        assert_eq!(op_str, "*");
+
+        let lval_str = "new";
+        assert_eq!(lval_str, "old");
+    }
+
+    #[test]
+    fn test_parse_starting_items() {
+        let starting_items = starting_items_from("Starting items: 74");
+        let expected = vec![74];
+        assert_eq!(starting_items, expected);
+
+        let starting_items = starting_items_from("Starting items: 79, 98");
+        let expected = vec![79, 98];
+        assert_eq!(starting_items, expected);
     }
 
     #[test]
