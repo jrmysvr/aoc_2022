5d6d67f79329d9281aebbb50ba2333aadb221e68
Jeremy <jeremy.savor@gmail.com>
2022-12-14 10:25:43 +0100

    Still trying to figure out part 2...

--- aoc_2022_python/day11.py
+++ aoc_2022_python/day11.py
@@ -1,4 +1,7 @@
 from aoc import input_fpath_for_day
+from queue import Queue, Empty
+from collections import OrderedDict
+from multiprocessing import Pool
 
 
 def add_to_worry(rval):
@@ -32,7 +35,11 @@ def square_worry():
 class Monkey:
     def __init__(self, monkey_id, items, operation, test_val, monkey_true, monkey_false):
         self.id = monkey_id
-        self.items = items
+        self._items = Queue()
+
+        for item in items:
+            self._items.put(item)
+
         self.operation = operation
         self.test_val = test_val
         self.monkey_true = monkey_true
@@ -45,6 +52,25 @@ class Monkey:
 
     def check(self, worry):
         return self.monkey_true if worry % self.test_val == 0 else self.monkey_false
+
+    def catch(self, item):
+        self._items.put(item)
+
+    def get_next_item(self):
+        try:
+            return self._items.get(block=False)
+        except Empty:
+            return None
+
+    @property
+    def items(self):
+        _items = list()
+        item = self.get_next_item()
+        while item is not None:
+            _items.append(item)
+            item = self.get_next_item()
+
+        return _items
 
 
 def monkey_from(block):
@@ -77,32 +103,104 @@ def parse_input(puzzle_input):
 
 def part_1(puzzle_input):
     monkeys = parse_input(puzzle_input)
-    items_for_monkeys = dict()
+    _monkeys = {monkey.id: monkey for monkey in monkeys}
     n_rounds = 20
     for _round in range(n_rounds):
-        for monkey in monkeys:
-            monkey.items.extend(items_for_monkeys.pop(monkey.id, list()))
-            item = monkey.items.pop(0) if monkey.items else None
+        for _monkey in monkeys:
+            monkey = _monkeys[_monkey.id]
+            item = monkey.get_next_item()
             while item is not None:
                 worry_level = monkey.inspect(item)
                 worry_level //= 3
                 next_monkey_id = monkey.check(worry_level)
-                thrown = items_for_monkeys.setdefault(next_monkey_id, list())
-                thrown.append(worry_level)
-                items_for_monkeys[next_monkey_id] = thrown
-                item = monkey.items.pop(0) if monkey.items else None
+                _monkeys[next_monkey_id].catch(worry_level)
+                item = monkey.get_next_item()
+
+    print([monkey.inspection_count for monkey in monkeys])
+    a, b = sorted([monkey.inspection_count for monkey in monkeys], reverse=True)[:2]
+    return a * b
+
+
+def brute_force_part_2(puzzle_input):
+    monkeys = parse_input(puzzle_input)
+    all_items = list()
+    for monkey in monkeys:
+        for item in monkey.items:
+            all_items.append(item)
+            ix = len(all_items) - 1
+            monkey.catch(ix)
+
+    _monkeys = {monkey.id: monkey for monkey in monkeys}
+
+    n_rounds = 10000
+    for _round in range(n_rounds):
+        for _monkey in monkeys:
+            monkey = _monkeys[_monkey.id]
+            item_ix = monkey.get_next_item()
+            while item_ix is not None:
+                item = all_items[item_ix]
+                worry_level = monkey.inspect(item)
+                all_items[item_ix] = worry_level
+                next_monkey_id = monkey.check(worry_level)
+                _monkeys[next_monkey_id].catch(item_ix)
+                item_ix = monkey.get_next_item()
 
     a, b = sorted([monkey.inspection_count for monkey in monkeys], reverse=True)[:2]
     return a * b
 
 
-def part_2(puzzle_input):
-    pass
-
-
-if __name__ == "__main__":
+def calc(item_and_ix):
+    _item_ix, item = item_and_ix
     with input_fpath_for_day(11).open() as f:
-        content = """\
+        puzzle_input = f.read()
+    monkeys = parse_input(puzzle_input)
+    all_items = list()
+    for monkey in monkeys:
+        for item in monkey.items:
+            all_items.append(item)
+            ix = len(all_items) - 1
+            if ix == _item_ix:
+                monkey.catch(ix)
+
+    _monkeys = OrderedDict([(monkey.id, monkey) for monkey in monkeys])
+
+    n_rounds = 10000
+    for _round in range(n_rounds):
+        for _monkey in monkeys:
+            monkey = _monkeys[_monkey.id]
+            item_ix = monkey.get_next_item()
+            while item_ix is not None:
+                item = all_items[item_ix]
+                worry_level = monkey.inspect(item)
+                all_items[item_ix] = worry_level
+                next_monkey_id = monkey.check(worry_level)
+                _monkeys[next_monkey_id].catch(item_ix)
+                item_ix = monkey.get_next_item()
+
+    return [monkey.inspection_count for monkey in _monkeys.values()]
+
+
+def part_2(puzzle_input):
+    all_items = list()
+    monkeys = parse_input(puzzle_input)
+    for monkey in monkeys:
+        for item in monkey.items:
+            all_items.append(item)
+            ix = len(all_items) - 1
+            monkey.catch(ix)
+
+    all_items = enumerate(all_items)
+    with Pool(10) as p:
+        counts = [0] * len(monkeys)
+        for result in p.map(calc, all_items):
+            counts = [c + r for (c, r) in zip(counts, result)]
+
+    a, b = sorted(counts, reverse=True)[:2]
+    return a * b
+
+
+def test_content():
+    return """\
 Monkey 0:
   Starting items: 79, 98
   Operation: new = old * 19
@@ -131,6 +229,9 @@ Monkey 3:
     If true: throw to monkey 0
     If false: throw to monkey 1"""
 
+
+if __name__ == "__main__":
+    with input_fpath_for_day(11).open() as f:
         content = f.read()
         print("Day 11")
         print("Part 1:", part_1(content))
